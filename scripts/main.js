a643a12db2d31fac592b7b4a9a50f3437d3e44c0
yordanzhelevdev <yordanzhelevdev@gmail.com>
2020-03-26 11:44:45 +0200

    Slide toggle fix

--- scripts/main.js
+++ scripts/main.js
@@ -14,13 +14,18 @@
 function checkFull(currProd) {
     // console.log(currProd.find('.prod-block__box--checked').length);
     if (currProd.find('.js-status-change').length === currProd.find('.prod-block__box--checked').length) {
-        $(currProd).addClass('prod-block--ready js-prod-toggle');
+        $(currProd).addClass('prod-block--ready');
+        $(currProd).find('.prod-block__text').addClass('js-prod-toggle');
         $(currProd).find('.row').slideUp('slow');
         $('.js-prod-toggle').on('click', function () {
-            $(this).find('.row').slideToggle();
+            $(this).next('.row').slideToggle();
         });
     }
 }
+
+$('.prod-block').each(function(){
+    checkFull($(this));
+});
 
 // Смяна от крояч на шивач и обратно
 $('input[name="filter-role"]').on('change', function() {
